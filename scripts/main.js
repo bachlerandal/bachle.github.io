1a1f2e72b6b1fab6b029343ed181ffef7b473661
Randal Bachle <rbachle@unomaha.edu>
2019-12-19 11:44:10 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -27,14 +27,15 @@ myButton.onclick = function() {
   setUserName();
 }
 
-/* Set the width of the side navigation to 250px */
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
 }
 
-/* Set the width of the side navigation to 0 */
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
-
 
